f5110ddf2ee633405e8677588cb4d88be4f9fc96
baiiab <leizhou_cwz@163.com>
2019-01-03 14:46:28 +0800

    修改

--- application/admin/controller/AdminUser.php
+++ application/admin/controller/AdminUser.php
@@ -26,64 +26,64 @@ class AdminUser extends Controller
 
 //    protected static $blacklist = ['delete', 'clear', 'deleteforever', 'recyclebin', 'recycle'];
 
-    protected function filter(&$map)
-    {
-        //不查询管理员
-        $map['id'] = ["gt", 1];
+    // protected function filter(&$map)
+    // {
+    //     //不查询管理员
+    //     $map['id'] = ["gt", 1];
 
-        if ($this->request->param('realname')) {
-            $map['realname'] = ["like", "%" . $this->request->param('realname') . "%"];
-        }
-        if ($this->request->param('account')) {
-            $map['account'] = ["like", "%" . $this->request->param('account') . "%"];
-        }
-        if ($this->request->param('email')) {
-            $map['email'] = ["like", "%" . $this->request->param('email') . "%"];
-        }
-        if ($this->request->param('mobile')) {
-            $map['mobile'] = ["like", "%" . $this->request->param('mobile') . "%"];
-        }
-    }
+    //     if ($this->request->param('realname')) {
+    //         $map['realname'] = ["like", "%" . $this->request->param('realname') . "%"];
+    //     }
+    //     if ($this->request->param('account')) {
+    //         $map['account'] = ["like", "%" . $this->request->param('account') . "%"];
+    //     }
+    //     if ($this->request->param('email')) {
+    //         $map['email'] = ["like", "%" . $this->request->param('email') . "%"];
+    //     }
+    //     if ($this->request->param('mobile')) {
+    //         $map['mobile'] = ["like", "%" . $this->request->param('mobile') . "%"];
+    //     }
+    // }
 
     /**
     
      * 修改密码
      */
-    public function password()
-    {
-        $id = $this->request->param('id/d');
-        if ($this->request->isPost()) {
-            //禁止修改管理员的密码，管理员id为1
-            if ($id < 2) {
-                return ajax_return_adv_error("缺少必要参数");
-            }
+    // public function password()
+    // {
+    //     $id = $this->request->param('id/d');
+    //     if ($this->request->isPost()) {
+    //         //禁止修改管理员的密码，管理员id为1
+    //         if ($id < 2) {
+    //             return ajax_return_adv_error("缺少必要参数");
+    //         }
 
-            $password = $this->request->post('password');
-            if (!$password) {
-                return ajax_return_adv_error("密码不能为空");
-            }
-            if (false === Loader::model('AdminUser')->updatePassword($id, $password)) {
-                return ajax_return_adv_error("密码修改失败");
-            }
-            return ajax_return_adv("密码已修改为{$password}", '');
-        } else {
-            // 禁止修改管理员的密码，管理员 id 为 1
-            if ($id < 2) {
-                throw new Exception("缺少必要参数");
-            }
+    //         $password = $this->request->post('password');
+    //         if (!$password) {
+    //             return ajax_return_adv_error("密码不能为空");
+    //         }
+    //         if (false === Loader::model('AdminUser')->updatePassword($id, $password)) {
+    //             return ajax_return_adv_error("密码修改失败");
+    //         }
+    //         return ajax_return_adv("密码已修改为{$password}", '');
+    //     } else {
+    //         // 禁止修改管理员的密码，管理员 id 为 1
+    //         if ($id < 2) {
+    //             throw new Exception("缺少必要参数");
+    //         }
 
-            return $this->view->fetch();
-        }
-    }
+    //         return $this->view->fetch();
+    //     }
+    // }
 
     /**
      * 禁用限制
      */
-    protected function beforeForbid()
-    {
-        // 禁止禁用 Admin 模块,权限设置节点
-        $this->filterId(1, '该用户不能被禁用', '=');
-    }
+    // protected function beforeForbid()
+    // {
+    //     // 禁止禁用 Admin 模块,权限设置节点
+    //     $this->filterId(1, '该用户不能被禁用', '=');
+    // }
 
       /**
      * 本页面的导航
@@ -91,6 +91,7 @@ class AdminUser extends Controller
      */
     public function nav()
     {
+        print_r(12131);exit();
         $nav =array();
         // $nav[]=array(
         //     'name'=>'考核',
